05ba9836a2ab039805c39ab8dc9fabf42517cac3
Lionel Lewis <lionellewis.triageio@gmail.com>
2021-09-12 10:29:58 -0500

    Made import section a little prettier

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,12 @@
-import mongoose from 'mongoose';
-import express from 'express';
-import cors from 'cors';
-import passport from 'passport';
+import mongoose      from 'mongoose';
+import express       from 'express';
+import cors          from 'cors';
+import passport      from 'passport';
 import passportLocal from 'passport-local';
-import cookieParser from 'cookie-parser';
-import session from 'express-session';
-import bcrypt from 'bcryptjs';
-import dotenv from 'dotenv';
+import cookieParser  from 'cookie-parser';
+import session       from 'express-session';
+import bcrypt        from 'bcryptjs';
+import dotenv        from 'dotenv';
 
 mongoose.connect("mongodb+srv://lionellewis:z3n_C0n50rt1um@triage-io.vwadk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     (err: Error) => {

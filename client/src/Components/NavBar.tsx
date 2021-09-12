c821cdb53d50beabd750e6ebbe6de85bf3998df8
Lionel Lewis <lionellewis.triageio@gmail.com>
2021-09-12 14:09:17 -0500

    started working on navbar component

--- client/src/Components/NavBar.tsx
+++ client/src/Components/NavBar.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import '../Pages/Styles/navbar.css'
 
 export default function NavBar() {
   return (
-    <div>
-      
-    </div>
+    <body>
+      <nav>
+        <a>
+          <Link to='home'>
+            Home
+          </Link>
+        </a>
+        <a>About</a>
+        <a>Blog</a>
+        <a>Portfolio</a>
+        <a>Contact</a>
+        <div className="animation start-home"></div>
+      </nav>
+    </body>
   );
 }

8b3b521f1c73c22d38efa56fffbf023ece5c4fa6
Lionel Lewis <lionellewis.triageio@gmail.com>
2021-09-12 10:41:47 -0500

    Implemented route for registration page

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,13 @@
-import mongoose      from 'mongoose';
-import express       from 'express';
-import cors          from 'cors';
-import passport      from 'passport';
-import passportLocal from 'passport-local';
-import cookieParser  from 'cookie-parser';
-import session       from 'express-session';
-import bcrypt        from 'bcryptjs';
-import dotenv        from 'dotenv';
-import User          from './models/User'
+import mongoose                       from 'mongoose';
+import express, { Request, Response } from 'express';
+import cors                           from 'cors';
+import passport                       from 'passport';
+import passportLocal                  from 'passport-local';
+import cookieParser                   from 'cookie-parser';
+import session                        from 'express-session';
+import bcrypt                         from 'bcryptjs';
+import dotenv                         from 'dotenv';
+import User                           from './models/User'
 
 mongoose.connect("mongodb+srv://lionellewis:z3n_C0n50rt1um@triage-io.vwadk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     (err: Error) => {
@@ -38,3 +38,16 @@ app.use(
 app.use(cookieParser());
 app.use(passport.initialize());
 app.use(passport.session());
+
+
+/**** Routes ****/
+app.post('/register', async (req: Request, res: Response) => {
+    // username, password
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const newUser = new User({
+        username: req.body.username,
+        password: hashedPassword
+    });
+    await newUser.save();
+    res.send('Success')
+});

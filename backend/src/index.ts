b3344e55e806bce81795a6dff99db38eb2880e6b
Lionel Lewis <lionellewis.triageio@gmail.com>
2021-09-12 11:53:59 -0500

    Implemented passport functionality

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,6 +8,9 @@ import session from 'express-session';
 import bcrypt from 'bcryptjs';
 import dotenv from 'dotenv';
 import User from './Models/User'
+import { Register } from './Pages/Register';
+
+const LocalStrategy = passportLocal.Strategy
 
 mongoose.connect("mongodb+srv://lionellewis:z3n_C0n50rt1um@triage-io.vwadk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     (err: Error) => {
@@ -40,6 +43,37 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
+/**** Passport ****/
+passport.use(new LocalStrategy((username, password, done) => {
+    User.findOne({ username : username }, (err, user : any) => {
+        if (err) throw err;
+        if (!user) return done(null, false);
+        bcrypt.compare(password, user.password, (err, result) => {
+            if (err) throw err;
+            if (result == true) {
+                return done(null, user)
+            } else {
+                return done(null, false)
+            }
+        });
+    });
+})
+);
+
+passport.serializeUser((user : any, cb) => {
+    cb(null, user.id);
+});
+passport.deserializeUser((id : string, cb) => {
+    User.findOne({_id: id}, (err, user : any) => {
+        const userInformation = {
+            username: user.username,
+            isAdmin: user.isAdmin
+        };
+        cb(err, userInformation)
+    });
+});
+
+
 /**** Routes ****/
 app.post('/register', async (req: Request, res: Response) => {
 
@@ -50,7 +84,7 @@ app.post('/register', async (req: Request, res: Response) => {
         return;
     }
 
-    User.findOne({ username }, async (err : Error, doc) => {
+    User.findOne({ username }, async (err : Error, doc : Register) => {
         if (err) throw err;
         if (doc) res.send('User already exists');
         if (!doc) {

b8e8f63d18ebdd22844c807c207c5e76979a541b
Lionel Lewis <lionellewis.triageio@gmail.com>
2021-09-12 10:18:28 -0500

    Initialized necessary middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,31 @@ import bcrypt from 'bcryptjs';
 import dotenv from 'dotenv';
 
 mongoose.connect("mongodb+srv://lionellewis:z3n_C0n50rt1um@triage-io.vwadk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-(err: Error) => {
-    if (err) throw err;
-    console.log("🚀 Connected to MongoDB");
-});
+    (err: Error) => {
+        if (err) throw err;
+        console.log("🚀 Connected to MongoDB");
+    }
+);
+
+/**** Middleware ****/
+const app = express();
+app.use(express.json());
+
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        credentials: true
+    })
+);
+
+app.use(
+    session({
+        secret: "secretcode",
+        resave: true,
+        saveUninitialized: true,
+    })
+);
+
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());

44a4a7499c8faa4976bd8062d2f5dcb3e3058401
Lionel Lewis <lionellewis.triageio@gmail.com>
2021-09-12 12:36:34 -0500

    Removed unnecessary code left from setting up react

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./Components/NavBar";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <NavBar />
+        <Switch>
+          <Route path='/' exact component={Homepage}></Route>
+          <Route path='/admin' component={AdminPage}></Route>
+          <Route path='/login' component={Login}></Route>
+          <Route path='/profile' component={Profile}></Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
